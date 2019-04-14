4b8b6439309c6cf12fbd82c0ff1d6eb1687e4b7a
Afnan Mirza <afnanmirza1999@gmail.com>
2019-04-14 16:27:53 -0400

    Initial Commit

---
+++ synchronoss-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  synchronoss-challenge
+//
+//  Created by Afnan Mirza on 4/14/19.
+//  Copyright © 2019 Afnan Mirza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
